b848ff5aa25b64ee35bf737ec00dedf588204c0d
Luis Otávio Pilotto <luis_tavio@hotmail.com>
2025-03-07 23:54:27 -0300

    fix: className error

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ export function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center w-full h-screen">
-        <h1 className="text-2xl cursor-pointer">Hello, World!</h1>
-        <Button>Click me</Button>
+        <h1 className="text-2xl">Hello, World!</h1>
+        <Button className="cursor-pointer">Click me</Button>
       </div>
     </>
   );
